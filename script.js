255da1518ef509511314d83927861534944d6841
Jennifer Krieger <alacritydanger@gmail.com>
2022-08-02 14:25:18 -0500

    changes to myFetch

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ window.addEventListener("load", function() {
  });
    let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse = myFetch.planetsReturned;
+   let listedPlanetsResponse = myFetch(); 
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
